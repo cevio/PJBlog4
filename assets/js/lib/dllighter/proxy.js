a543ff04970b9e87a53f6dba6e294d9a917b15d5
cevio <evio@vip.qq.com>
2012-12-05 17:43:32 +0800

    update

--- assets/js/lib/dllighter/proxy.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-define(['assets/js/lib/dllighter/hl-all', 'assets/js/lib/dllighter/style.css'], function(){
-	$(function(){
-		DlHighlight.HELPERS.highlightByName("syntax", "pre");
-	});
-});
